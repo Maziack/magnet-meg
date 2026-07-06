0c77d870bf187eacad15df411ddfb1a96fba98ea
Maziack <mackenzie.kerman@gmail.com>
2026-07-05 19:50:14 -0600

    Added Raycasts

--- magnet-meg/magnet-meg/Scenes/Characters/Player/player.gd
+++ magnet-meg/magnet-meg/Scenes/Characters/Player/player.gd
@@ -3,7 +3,7 @@ extends Player
 func _ready() -> void:
 	pass
 
-func _physics_process(_delta: float) -> void:
+#func _physics_process(_delta: float) -> void:
 	#print("Y Velocity: ", velocity.y)
-	velocity = (velocity).clamp(Vector2(-1*max_velocity,-1*max_velocity),Vector2(max_velocity,max_velocity))
-	move_and_slide()
+	#velocity = (velocity).clamp(Vector2(-1*max_velocity,-1*max_velocity),Vector2(max_velocity,max_velocity))
+	#move_and_slide()
